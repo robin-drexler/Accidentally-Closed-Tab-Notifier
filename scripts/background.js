62ffb386104f24842b59949e1cf6d36843c108f3
Robin Drexler <drexler.robin@gmail.com>
2012-12-14 13:55:08 +0100

    first soldered version, which measures time on tab

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,50 @@
-var settings = new Store("settings", {
-    "sample_setting": "This is how you use Store.js to remember values"
+var tabs = {},
+    timer;
+
+
+function newTab() {
+    var defaults = {
+        seconds: 0
+    }
+    return defaults;
+}
+
+
+chrome.tabs.onCreated.addListener(function (tab) {
+
+    tabs[tab.id] = newTab();
+    //console.log(tabs);
 });
 
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse(settings.toObject());
-  });
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+    var currentTab = tabs[tabId] || newTab();
+
+    if (info.status !== "loading") {
+        return;
+    }
+
+    currentTab.url = info.url;
+    tabs[tabId] = currentTab;
+});
+
+chrome.tabs.onActivated.addListener(function (tab) {
+    var currentTab = tabs[tab.tabId] || newTab(),
+        seconds;
+
+    window.clearInterval(timer);
+
+    timer = window.setInterval(function () {
+        currentTab.seconds += 1;
+        tabs[tab.tabId] = currentTab;
+
+    }, 1000);
+});
+
+chrome.tabs.onRemoved.addListener(function (tabId) {
+    var currentTab = tabs[tabId] || newTab();
+
+    if (currentTab.seconds <= 10) {
+        console.log("OMG");
+    }
+
+});
